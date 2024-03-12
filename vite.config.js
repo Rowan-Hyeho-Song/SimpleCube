8eea6dc8c7313f0983694529eddf4cf2c336d47a
Rowan-Hyeho-Song <dev.rowansong@gmail.com>
2024-03-12 11:38:05 +0900

    Set Project Structure

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    base: "/SimpleCube/",
+    resolve: {
+        alias: [
+            { find: "@", replacement: "/src" },
+            { find: "@api", replacement: "/src/api" },
+            { find: "@assets", replacement: "/src/assets" },
+            { find: "@components", replacement: "/src/components" },
+            { find: "@constants", replacement: "/src/constants" },
+            { find: "@hooks", replacement: "/src/hooks" },
+            { find: "@pages", replacement: "/src/pages" },
+            { find: "@styles", replacement: "/src/styles" },
+            { find: "@utils", replacement: "/src/utils" },
+        ],
+    },
+});

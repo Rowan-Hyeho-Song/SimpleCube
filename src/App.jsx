d28b790b4711462ceb03bda9e46e9e1e20ac6a5e
Rowan-Hyeho-Song <dev.rowansong@gmail.com>
2024-03-14 05:16:17 +0900

    Update App.jsx
    - Context Provider들을 하나로 합쳐서 내보내는 코드 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
-import { ThemeProvider } from '@utils/ThemeProvider';
-import Cube from "@components/Cube";
+import { createElement } from "react";
+import { ThemeProvider } from "@hooks/ThemeProvider";
+import { SettingProvider } from "@hooks/SettingProvider";
+import SettingBar from "@components/setting/SettingBar";
+import MainView from "@components/layout/MainView";
+
+// Context들을 합쳐서 적용
+const AppProvider = ({ contexts, children }) => contexts.reduce(
+    (acc, cur) => createElement(cur, { children: acc }),
+    children
+);
 
 function App() {
     return (
-        <ThemeProvider>
-            <Cube />
-        </ThemeProvider>
+        <AppProvider contexts={[SettingProvider, ThemeProvider]}>
+            <SettingBar />
+            <MainView />
+        </AppProvider>
     );
 }
 
---
+++ src/components/layout/MainView.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import CubeContainer from "@components/cube/CubeContainer";
+
+const MainContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    background-color: ${({theme}) => theme.background};
+`;
+
+function MainView() {
+    return (
+        <MainContainer></MainContainer>
+    );
+}
+
+export default MainView;

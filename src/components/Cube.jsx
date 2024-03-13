509b29f1bf61375ae7d6a4c5378cbbb20ab77afb
Rowan-Hyeho-Song <dev.rowansong@gmail.com>
2024-03-13 18:10:37 +0900

    Add ThemeProvider

---
+++ src/components/Cube.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+function Cube() {
+    return (
+        <></>
+    );
+}
+
+export default Cube;
---
+++ src/constants/theme.jsx
@@ -0,0 +1,7 @@
+export const dark = {
+
+};
+
+export const light = {
+
+};

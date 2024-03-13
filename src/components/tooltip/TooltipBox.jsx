6bafe4600a961d69f7a1df7de2ea1c747ecb5272
Rowan-Hyeho-Song <dev.rowansong@gmail.com>
2024-03-14 07:17:05 +0900

    Create Tooltip
    - 툴팁 기능 생성

---
+++ src/components/tooltip/TooltipBox.jsx
@@ -0,0 +1,75 @@
+import styled, { keyframes } from "styled-components";
+
+const popup = keyframes`
+0% {
+    opacity: 0;
+}
+100% {
+    opacity: 1;
+}
+`;
+
+const Container = styled.div`
+    position: absolute;
+    background-color: #000000;
+    color: #ffffff;
+    padding: 5px 10px;
+    border-radius: 4px;
+    pointer-events: none;
+
+    animation: ${popup} 0.3s forwards;
+
+    &.TopLeft {
+        transform: translate(0, -100%);
+    }
+    &.Top {
+        transform: translate(-50%, -100%);
+    }
+    &.TopRight {
+        transform: translate(-100%, -100%);
+    }
+
+    &.LeftTop {
+        transform: translate(-100%, 0);
+    }
+    &.Left {
+        transform: translate(-100%, -50%);
+    }
+    &.LeftBottom {
+        transform: translate(-100%, -100%);
+    }
+
+    &.RightTop {
+        transform: translate(0, 0);
+    }
+    &.Right {
+        transform: translate(0, -50%);
+    }
+    &.RightBottom {
+        transform: translate(0, -100%);
+    }
+
+    &.BottomLeft {
+        transform: translate(0, 0);
+    }
+    &.Bottom {
+        transform: translate(-50%, 0);
+    }
+    &.BottomRight {
+        transform: translate(-100%, 0);
+    }
+`;
+
+function TooltipBox({
+    style,
+    message,
+    direction
+})  {
+    return (
+        <Container className={direction} style={style}>
+            {message}
+        </Container>
+    );
+}
+
+export default TooltipBox;
